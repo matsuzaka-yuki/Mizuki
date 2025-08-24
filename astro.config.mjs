c90115e989f48014e3ceda2ae3ab12705e3b8c24
ChuwuYo <141227996+ChuwuYo@users.noreply.github.com>
2025-08-25 00:39:13 +0800

    配置 GitHub Pages 部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mizuki.mysqil.com/",
-
-	base: "/",
+	// GitHub Pages 配置
+	site: "https://chuwuyo.github.io",
+	base: "/Chuwu",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
