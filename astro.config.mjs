65275b4bb5da798d51f50c341540e97d31529dd0
LittleSadSheep <2749533087@qq.com>
2025-08-28 21:02:51 +0800

    feat: 非正确域名弹窗

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mizuki.mysqil.com/",
-
+	allowedHosts: true,
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
