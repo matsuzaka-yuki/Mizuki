70b1af8d81eacf748ea029f589c2f0625e3fda5f
FUSEAKIRA <fuseakira1908@gmail.com>
2025-09-03 20:15:16 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "Fuseakira",
-	subtitle: "One demo website",
+	subtitle: "MY DEMO WEBSITE",
 
 	lang: SITE_LANG,
 
