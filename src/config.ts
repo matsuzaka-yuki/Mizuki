991a5b7e23d57981366366cd5b372ccaaa351c33
FUSEAKIRA <fuseakira1908@gmail.com>
2025-09-04 11:33:05 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -247,7 +247,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.gif", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "FUSEAKIRA",
 	bio: "Ho Fatto Un Sogno",
 	links: [
